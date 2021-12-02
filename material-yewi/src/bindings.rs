65d235a5f4d8e2104d1b1af001b9c8e0085347ce
Martin Molzer <ga65guy@mytum.de>
2021-12-02 14:18:10 +0100

    use gloo for timeout bindings

--- material-yewi/src/bindings.rs
+++ material-yewi/src/bindings.rs
@@ -1,30 +1,10 @@
-use wasm_bindgen::{closure::Closure, prelude::wasm_bindgen};
+use gloo::timers::callback::Timeout;
 
 #[derive(Debug)]
-pub struct TimeoutHandle(i32, Closure<dyn FnMut()>);
+pub struct TimeoutHandle(Timeout);
 
-impl Drop for TimeoutHandle {
-    fn drop(&mut self) {
-        do_clear_timeout(self.0);
-    }
-}
-
-#[wasm_bindgen(inline_js = r#"
-export function do_set_timeout(handler, delay) {
-    return setTimeout(handler, delay);
-}
-export function do_clear_timeout(timeoutid) {
-    clearTimeout(timeoutid);
-}
-"#)]
-extern "C" {
-    fn do_set_timeout(handler: &Closure<dyn FnMut()>, time: i32) -> i32;
-    fn do_clear_timeout(timeoutid: i32);
-}
-
-pub fn set_timeout(handler: impl 'static + FnMut(), delay_millis: i32) -> TimeoutHandle {
-    let closure = Closure::wrap(Box::new(handler) as Box<dyn FnMut()>);
-    TimeoutHandle(do_set_timeout(&closure, delay_millis), closure)
+pub fn set_timeout(handler: impl 'static + FnOnce(), delay_millis: u32) -> TimeoutHandle {
+    TimeoutHandle(Timeout::new(delay_millis, handler))
 }
 
 pub fn clear_timeout(timeout: TimeoutHandle) {
